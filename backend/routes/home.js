cf23d8918ae2ffa6dff8986107891aa48cbb0b50
upparkrishna032-ops <upparkrishna032@gmail.com>
2025-10-25 22:55:30 +0530

    Chnaged server file

--- backend/routes/home.js
+++ backend/routes/home.js
@@ -5,10 +5,3 @@ const router = express.Router();
 router.get("api", getHome);
 
 export default router;
-
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/upparkrishna032-ops/cicd.git
-git push -u origin main
